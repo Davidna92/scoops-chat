b29ec0a759674d771d7b774e3673da600fe48cb4
David Nahum <63794877+Davidna92@users.noreply.github.com>
2020-11-03 14:12:57 +0200

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -7,14 +7,14 @@ import "firebase/storage";
 
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBIvBisnOVtcq4kBTgYUomWReXMp630EwM",
-  authDomain: "news-14b77.firebaseapp.com",
-  databaseURL: "https://news-14b77.firebaseio.com",
-  projectId: "news-14b77",
-  storageBucket: "news-14b77.appspot.com",
-  messagingSenderId: "636466269581",
-  appId: "1:636466269581:web:5cabe79d962e204cfd41c4",
-  measurementId: "G-DC0C7LW0Y6"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  measurementId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 });
 
 export const db = app.firestore();
